df09e7946d1b39da4d08777eb8a0c0ace271d452
haleyt <haleyt@wit.edu>
2018-01-29 14:17:23 -0500

    Created log in page

---
+++ Signapp/student_database.py
@@ -0,0 +1,10 @@
+#File that stores the database containing the names of each student
+
+student_dict = {
+
+    "Steve Sica" : "",
+    "Terry Swan" : "",
+    "Eric Keeley" : "",
+    "Matthew Gonzalez" : "",
+    "Alex Bayoff" : ""
+}
--- Signapp/urls.py
+++ Signapp/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from Signapp import views
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
+    url(r'^$', views.StartPageView.as_view()),
+    url(r'^index/$', views.HomePageView.as_view()),
     url(r'^about/$', views.AboutPageView.as_view()),
 ]
